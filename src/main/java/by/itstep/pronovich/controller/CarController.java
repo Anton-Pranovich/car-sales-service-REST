603127ef81b095de3c5ced5296cdb232a374472f
Anton-Pranovich <anton.pronovich@gmail.com>
2022-02-26 14:09:25 +0300

    added dependency spring-boot-starter=hateoas

--- src/main/java/by/itstep/pronovich/controller/CarController.java
+++ src/main/java/by/itstep/pronovich/controller/CarController.java
@@ -2,7 +2,9 @@ package by.itstep.pronovich.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import by.itstep.pronovich.exception.CarNotFoundException;
 import by.itstep.pronovich.model.Car;
 import by.itstep.pronovich.repository.CarRepository;
+
 
 @RestController
 public class CarController {
@@ -30,6 +33,10 @@ public class CarController {
 		return repository.findAll();
 	}
 
+	@GetMapping("/cars")
+	List<Car> allByGroup() {
+		return repository.findAll();
+	}
 
 	@PostMapping("/cars")
 	Car newCar(@RequestBody Car newCar) {
@@ -42,6 +49,17 @@ public class CarController {
 	Car one(@PathVariable Long id) {
 		return repository.findById(id).orElseThrow(() -> new CarNotFoundException(id));
 	}
+
+//	@GetMapping("/cars/{id}")
+//	EntityModel<Car> oneCar(@PathVariable Long id) {
+//
+//		Car car = repository.findById(id) //
+//				.orElseThrow(() -> new CarNotFoundException(id));
+//
+//		return EntityModel.of(car, 
+//				linkTo(methodOn(CarController.class).oneCar(id)).withSelfRel(),
+//				linkTo(methodOn(CarController.class).all()).withRel("cars"));
+//	}
 
 	@PutMapping("/cars/{id}")
 	Car replaceEmployee(@RequestBody Car newCar, @PathVariable Long id) {
